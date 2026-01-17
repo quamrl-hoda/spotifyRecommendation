248d304044a5e6c17fa5ab417ca2cb176c4277e9
quamrul hoda <qhoda489@gmail.com>
2026-01-17 21:20:33 +0530

    folder structure updated

--- template.py
+++ template.py
@@ -19,6 +19,8 @@ list_of_files = [
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/entity/config_entity.py",
     f"src/{project_name}/constants/__init__.py",
+    f"src/{project_name}/logger/__init__.py",
+    f"src/{project_name}/exception/__init__.py",
     "config/config.yaml",
     "params.yaml",
     "app.py",
